0e0e1a4845007bc05f112e88650e83545bc79242
서광명 <auddlzz000@gmail.com>
2023-04-21 06:14:44 +0000

    Update 9 files

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("hello")
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import  ActionChains
+
+driver = webdriver.Chrome()
+url = 'https://sir.kr'
+driver.get(url)            # url 오픈해라
+driver.maximize_window()   #창 크게 만들기
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+
+driver = webdriver.Chrome()
+url = 'https://sir.kr'
+driver.get(url)            # url 오픈해라
+driver.maximize_window()  # 창 크게 만들기
